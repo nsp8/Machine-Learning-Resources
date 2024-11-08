eac5d6e5fb8701b742800de0d57c4a7d0f10e5d2
Nishant Parmar <nishant.singhparmar@ge.com>
2024-11-08 15:23:02 -0500

    Updated model to generate content from trained tokens; added utilities

--- nano_gpt/train.py
+++ nano_gpt/train.py
@@ -29,12 +29,12 @@ class DataLoader:
             raise ValueError("split ratio must be within 0.5 and 1.")
 
     def get_chunks(
-            self,
-            split: DataSplitType,
-            *,
-            chunk_size: int = 8,
-            batch_size: int = 4,
-            visualize_tokenized_chunks: bool = True
+        self,
+        split: DataSplitType,
+        *,
+        chunk_size: int = 8,
+        batch_size: int = 4,
+        visualize_tokenized_chunks: bool = True
     ) -> tuple[Tensor, Tensor]:
         def _get_batch(data):
             # random offsets: generate batch_size number of random offsets
@@ -61,37 +61,71 @@ class BigramLanguageModel(Module):
         super().__init__()
         self.token_embedding_table = Embedding(vocab_size, vocab_size)
 
-    def forward(self, idx, targets):
+    def forward(self, idx, targets=None):
         # (batch * time * channel),
         # where time is the chunk size and channel is the vocab size
         logits = self.token_embedding_table(idx)
         # Loss tells the quality of the logits
         # PyTorch expects the multidimensional inputs
         # to CrossEntropyLoss as (batch * channel * time)
-        _batch, _time, _channel = logits.shape
-        logits = logits.view(_batch * _time, _channel)
-        targets = targets.view(_batch * _time)
-        loss = functional.cross_entropy(logits, targets)
+        loss = None
+        if targets is not None:
+            _batch, _time, _channel = logits.shape
+            logits = logits.view(_batch * _time, _channel)
+            targets = targets.view(_batch * _time)
+            loss = functional.cross_entropy(logits, targets)
         return logits, loss
+
+    def generate(self, idx, max_new_tokens):
+        """
+        Generates predictions, for each batch.
+        (B * T) -> (B * T+1), (B * T+2), ...
+        """
+        # idx is (batch * time) array of indices in the current context
+        for _ in range(max_new_tokens):
+            # get the predictions
+            logits, loss = self(idx)
+            # focus only on the last time step
+            logits = logits[:, -1, :]  # (batch * channel)
+            # apply softmax to get probabilities
+            probs = functional.softmax(logits, dim=-1)  # (batch * channel)
+            # sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1)  # (batch * 1)
+            # append sampled index to the running sequence
+            idx = torch.cat((idx, idx_next), dim=1)  # (batch * time+1)
+        return idx
+
+
+def get_prediction(
+    tokenizer: CharacterLevelTokenizer,
+    model: BigramLanguageModel,
+):
+    prediction = model.generate(
+        torch.zeros((1, 1), dtype=torch.long),
+        max_new_tokens=100
+    )[0].tolist()
+    return tokenizer.decode(prediction)
 
 
 def test_bigram_model():
-    data: CharacterLevelTokenizer | None = convert_test_file_to_tensors()
-    if data:
-        tensors = data.tokens
+    tokenizer: CharacterLevelTokenizer | None = convert_test_file_to_tensors()
+    if tokenizer:
+        tensors = tokenizer.tokens
         if tensors is not None and isinstance(tensors, Tensor):
             data_loader = DataLoader(tensors)
             train_batch = data_loader.get_chunks(
                 DataSplitType.TRAIN, visualize_tokenized_chunks=False
             )
-            vocab_size = data.n_vocab
+            vocab_size = tokenizer.n_vocab
             if vocab_size > 0:
-                model = BigramLanguageModel(vocab_size=data.n_vocab)
+                model = BigramLanguageModel(vocab_size=tokenizer.n_vocab)
                 logits, loss = model(*train_batch)
                 # for a vocab_size, the Log Likelihood Estimate:
                 # -ln(1/vocab_size)
                 expected_log_loss = -log(1 / vocab_size, e)
+                prediction = get_prediction(tokenizer, model)
                 print(f"{logits.shape=} {loss=} {expected_log_loss=:.4f}")
+                print(f"{prediction=}")
             else:
                 raise ValueError("Vocab size has to be a positive integer.")
     else:
--- nano_gpt/utils.py
+++ nano_gpt/utils.py
@@ -3,23 +3,32 @@ from nano_gpt.tokenizer import CharacterLevelTokenizer
 from nano_gpt.constants import TEST_FILE_PATH
 
 TEST_FILE = ""
+DEFAULT_FILE_NAME = "input.txt"
 
 
-def download_test_file():
-    global TEST_FILE
-    file_name = Path(TEST_FILE_PATH).name
-    TEST_FILE = f"./{file_name}"
+def with_file(f):
+    def wrapper(*args, **kwargs):
+        global TEST_FILE
+        directory = Path(__file__).parent.absolute()
+        TEST_FILE = TEST_FILE or Path(f"{directory}/{DEFAULT_FILE_NAME}")
+        return f(*args, **kwargs)
+    return wrapper
+
+
+@with_file
+def download_test_file() -> bool:
     if not Path(TEST_FILE).exists():
         from subprocess import check_output
+        print(f"Downloading file into {TEST_FILE} ...")
         check_output(f"wget {TEST_FILE_PATH}", shell=True)
+        return True
     else:
         print("Test file already exists!")
+        return False
 
 
-def convert_test_file_to_tensors():
-    global TEST_FILE
-    directory = Path(__file__).parent.absolute()
-    TEST_FILE = TEST_FILE or Path(f"{directory}/input.txt")
+@with_file
+def convert_test_file_to_tensors() -> CharacterLevelTokenizer | None:
     if Path(TEST_FILE).exists():
         tokenizer = CharacterLevelTokenizer()
         with open(TEST_FILE) as f:
