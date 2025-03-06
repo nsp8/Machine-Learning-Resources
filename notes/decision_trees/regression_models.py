caa08d63b76236165b8c5c3d1d3f7349641ff47e
Nishant <55044190+nsp8@users.noreply.github.com>
2025-03-06 17:11:53 -0500

    Adding XGBoostRegressor

--- notes/decision_trees/regression_models.py
+++ notes/decision_trees/regression_models.py
@@ -1,13 +1,15 @@
 from dataclasses import dataclass
 from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import cross_val_score
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
+from xgboost import XGBRegressor
 from notes.data_loader import Dataset
 
 
 @dataclass
 class TreeBasedRegressor:
-    model: DecisionTreeRegressor | RandomForestRegressor
+    model: DecisionTreeRegressor | RandomForestRegressor | XGBRegressor
     dataset: Dataset
 
     def train(self):
@@ -19,4 +21,11 @@ class TreeBasedRegressor:
     def get_metrics(self, predictions):
         return {
             "mean_absolute_error": mean_absolute_error(self.dataset.y_val, predictions),
+            "cross_validation_scores": -1 * cross_val_score(
+                self.model,
+                self.dataset.X,
+                self.dataset.y,
+                cv=5,
+                scoring="neg_mean_absolute_error"
+            ),
         }
